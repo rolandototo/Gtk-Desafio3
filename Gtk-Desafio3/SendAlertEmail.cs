e4331c6bd06eeaeca9025d828d39c86f557939cd
Josias <Josias_romero60@outlook.com>
2020-05-09 13:05:32 -0600

    Agregando Usuario.txt

--- Gtk-Desafio3/SendAlertEmail.cs
+++ Gtk-Desafio3/SendAlertEmail.cs
@@ -23,17 +23,6 @@ namespace GtkDesafio3
             SmtpServer.Send(mail);
 
         }
-        private string GetIP()
-        {
-            string strHostName = "";
-            strHostName = System.Net.Dns.GetHostName();
-
-            IPHostEntry ipEntry = System.Net.Dns.GetHostEntry(strHostName);
-
-            IPAddress[] addr = ipEntry.AddressList;
-
-            return addr[addr.Length - 1].ToString();
-
-        }
+        .
     }
 }
