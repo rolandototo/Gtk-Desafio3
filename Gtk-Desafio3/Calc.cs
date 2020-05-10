12e40dcfcc4aea18a5358676d63a7c7a29a53650
Rolando Alcedis Escobar Echevrria <rolandototo@gmail.com>
2020-05-09 18:07:25 -0600

    Assambly conflict Operaciones
    Operaciones => calc

--- Gtk-Desafio3/Calc.cs
+++ Gtk-Desafio3/Calc.cs
@@ -1,12 +1,7 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
 namespace GtkDesafio3
 {
-    class Operaciones
+    public class Calc
     {
         struct registro
         {
